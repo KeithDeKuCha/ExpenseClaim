e29bfb6d37a75801d105729b3126bed9e97070c6
yang <yang@yangs-MacBook-Pro.local>
2021-04-09 23:54:54 +0800

    json serializersettings of newtonsoft.json

--- ExpenseClaim/Startup.cs
+++ ExpenseClaim/Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 
 namespace ExpenseClaim
 {
@@ -16,14 +20,31 @@ namespace ExpenseClaim
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            fsql = new FreeSql.FreeSqlBuilder()
+            .UseConnectionString(FreeSql.DataType.MySql, @"Data Source=127.0.0.1;Port=3306;User ID=root;Password=sync877472,; Initial Catalog=bxdb;Charset=utf8; SslMode=none;Min pool size=1")
+            .UseLazyLoading(true)
+            .UseMonitorCommand(cmd => Console.WriteLine(cmd.CommandText))
+            .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。【开发环境使使用，正式环境记得注释】
+            .Build(); //请务必定义成 Singleton 单例模式
         }
 
         public IConfiguration Configuration { get; }
+        public IFreeSql fsql { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            //json格式设定，必须引入Microsoft.AspNetCore.Mvc.NewtonsoftJson
+            services.AddControllersWithViews().AddNewtonsoftJson(opt=> {
+                //驼峰命名字段名
+                opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                //忽略循环引用
+                opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                opt.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            });
+
+            services.AddSingleton<IFreeSql>(fsql);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
